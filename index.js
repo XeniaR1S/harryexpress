a68ab2495429a6065f59b3293f298436d813303f
XeniaR1S <lecomtep.x@gmail.com>
2020-01-02 14:31:29 +0100

    mySQL works

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const connection = require('./conf');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Get data
+app.listen(port, (err) => {
+  if (err) {
+    throw new Error('Something bad happened...');
+  }
+  console.log(`Server is listening on ${port}`);
+});
 
-app.get('/api/harry', (req, res) => {
-    connection.query('SELECT * from harry', (err, results) => {
-    });
+app.get('/api/wizard', (req, res) => {
+  connection.query('SELECT * from wizard', (err, results) => {
     if (err) {
       res.status(500).send('Erreur lors de la récupération des données, moldu.');
     } else {
       res.json(results);
     }
-  });
+    });
+});
+
---
+++ script.sql
@@ -0,0 +1,8 @@
+INSERT INTO wizard (name, birthday, wand_length, is_human) 
+VALUES ('Harry Potter', '1980-07-31', 11, true),
+('Hermione Granger', '1979-09-19', 10, true),
+('Ron Weasley', '1980-03-01', 14, true),
+('Draco Malfoy', '1980-06-05', 10, true),
+('Minerva McGonagall', '1925-10-04', 13, true),
+('Cedric Diggory', '1978-09-12', 11, true),
+('Severus Snape', '1960-01-09', 13, true);

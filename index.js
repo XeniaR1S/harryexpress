84288519c2d2affcee64bef91c8a0e5ef476bf34
XeniaR1S <lecomtep.x@gmail.com>
2020-01-02 16:58:34 +0100

    Finish

--- index.js
+++ index.js
@@ -26,3 +26,118 @@ app.get('/api/wizard', (req, res) => {
     });
 });
 
+app.get('/api/wizard/name', (req, res) => {
+  connection.query('SELECT name from wizard', (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données, moldu.');
+    } else {
+      res.json(results);
+    }
+    });
+});
+
+app.get('/api/wizard/birthday', (req, res) => {
+  connection.query('SELECT birthday from wizard', (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données, moldu.');
+    } else {
+      res.json(results);
+    }
+    });
+});
+
+app.get('/api/wizard/wand', (req, res) => {
+  connection.query('SELECT wand_length from wizard', (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données, moldu.');
+    } else {
+      res.json(results);
+    }
+    });
+});
+
+app.get('/api/wizard/ishuman', (req, res) => {
+  connection.query('SELECT is_human from wizard', (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données, moldu.');
+    } else {
+      res.json(results);
+    }
+    });
+});
+
+app.get('/api/wizard/wandl', (req, res) => {
+  connection.query('SELECT wand_length FROM wizard WHERE wand_length > 11 ORDER BY wand_length ASC ', (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données, moldu.');
+    } else {
+      res.json(results);
+    }
+    });
+});
+
+app.get('/api/wizard/filtername', (req, res) => {
+  connection.query('SELECT name FROM wizard WHERE name LIKE "%r" ', (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données, moldu.');
+    } else {
+      res.json(results);
+    }
+    });
+});
+
+app.post('/api/wizard/postname', (req, res) => {
+  const formData = req.body
+  connection.query('INSERT INTO wizard SET ? ', formData, (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données, moldu.');
+    } else {
+      res.json(results);
+    }
+    });
+});
+
+app.put('/api/wizard/modifyname/:id', (req, res) => {
+  const formData = req.body
+  const idWizard = req.params.id
+  connection.query('UPDATE wizard SET ? WHERE id = ?', [formData, idWizard], (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données, moldu.');
+    } else {
+      res.json(results);
+    }
+    });
+});
+
+app.put('/api/wizard/modifyrace/:id', (req, res) => {
+  const idWizard = req.params.id
+  connection.query('UPDATE wizard SET is_human = NOT is_human WHERE id = ?', [idWizard], (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données, moldu.');
+    } else {
+      res.json(results);
+    }
+    });
+});
+
+app.delete('/api/wizard/deleteuser/:id', (req, res) => {
+  const idWizard = req.params.id
+  connection.query('DELETE FROM `wizard` WHERE `id` = ? ', [idWizard], (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données, moldu.');
+    } else {
+      res.json(results);
+    }
+    });
+});
+
+app.delete('/api/wizard/deletefalse', (req, res) => {
+  const idWizard = req.params.id
+  connection.query('DELETE FROM `wizard` WHERE `is_human` = false ', [idWizard], (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données, moldu.');
+    } else {
+      res.json(results);
+    }
+    });
+});
